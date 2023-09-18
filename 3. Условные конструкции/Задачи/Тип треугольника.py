25c272ced1a55d69c41c4006d953a654b62a070d
Андрей Боев <a.boev.it@gmail.com>
2023-09-18 20:05:09 +0300

    Циклы

--- 3. Условные конструкции/Задачи/Тип треугольника.py
+++ 3. Условные конструкции/Задачи/Тип треугольника.py
@@ -2,16 +2,16 @@ first_side_triangle = int(input("Введите первую сторону тр
 second_side_triangle = int(input("Введите вторую сторону треугольника: "))
 third_side_triangle = int(input("Введите третью сторону треугольника: "))
 if first_side_triangle + second_side_triangle <= third_side_triangle or \
-    first_side_triangle + third_side_triangle <= second_side_triangle or \
-    second_side_triangle + third_side_triangle <= first_side_triangle:
+        first_side_triangle + third_side_triangle <= second_side_triangle or \
+        second_side_triangle + third_side_triangle <= first_side_triangle:
     print("Impossible")
 else:
     h = max(first_side_triangle, second_side_triangle, third_side_triangle)
     c1 = min(first_side_triangle, second_side_triangle, third_side_triangle)
     c2 = first_side_triangle + second_side_triangle + third_side_triangle - (h + c1)
-    if h**2 == c1**2 + c2**2:
+    if h ** 2 == c1 ** 2 + c2 ** 2:
         print("Rectangural")
-    elif h**2 > c1**2 + c2**2:
+    elif h ** 2 > c1 ** 2 + c2 ** 2:
         print("Obtuse")
     else:
         print("Acute")
