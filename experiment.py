dbf30ba14a2b6450ef66a203fd1ed43ff38b8901
Андрей Боев <a.boev.it@gmail.com>
2025-01-13 18:34:17 +0300

    Пример yield

--- experiment.py
+++ experiment.py
@@ -1,3 +1,4 @@
+"""
 from math import sqrt
 
 
@@ -9,3 +10,13 @@ def triangle_area(a, b, c):
 args = {'c': 12, 'b': 8, 'a': 7}
 
 print(triangle_area(**args))
+"""
+
+# Пример yield
+def get_aplph(n):
+    for i in range(n):
+        yield chr(32 + i)
+
+
+for c in get_aplph(10):
+    print(c, end=' ')
